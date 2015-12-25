8ec17053a789fbbef74e328ab6fe47bd04f71b78
Kazuho Oku <kazuhooku@gmail.com>
2015-12-25 13:28:51 +0900

    prefix include guard with `mruby_` to avoid potential collision

--- src/input_stream.h
+++ src/input_stream.h
@@ -2,8 +2,8 @@
  * input_stream.h
  *
  */
-#ifndef input_stream_h
-#define input_stream_h
+#ifndef mruby_input_stream_h
+#define mruby_input_stream_h
 
 mrb_value
 mrb_input_stream_value(mrb_state *mrb, char *base, mrb_int len);
